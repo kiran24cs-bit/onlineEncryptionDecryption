498b99aa2298b6d86b7d85a1a26df02f7916f94a
kiran <kiran24cs@rnsit.ac.in>
2026-03-17 23:42:48 +0530

    Refactor encryption and decryption logic
    Refactor encryption and decryption functions to support multiple iterations based on user input. Introduce codeenc and codedec functions for better structure.

--- another.js
+++ another.js
@@ -2,27 +2,53 @@ let keys = {'0':' ','1':'u','2':'x','3':'/','4':'+','5':'N','6':'h','7':'.','8':
 
 let symbols = {'0':'s','1':':','2':'{','3':'g','4':'D','5':'x','6':'m','7':'G','8':'t','9':'=','l':'`','u':'1','x':'2','/':'3','+':'4','N':'5','h':'6','.':'7','{':'8','n':'9',' ':'0','-':'-','A':'q','i':'w','@':'e','*':'r','%':'y','`':'u','M':'i','T':'o','g':'p','_':'[','a':']','=':'a','O':'d','~':'f','I':'h','q':'j',':':'k',';':'l','!':';','F':'\'','X':'z','d':'c','C':'v',')':'b','K':'n','&':',','U':'.','f':'/','b':' ','s':'~','j':'!','J':'@','P':'#','p':'$','^':'%','>':'^','c':'&','t':'*','<':'(','Z':')','w':'_','E':'+','D':'Q',',':'W','r':'E','$':'R','Y':'T','e':'Y','\'':'U','W':'I',']':'O','S':'P','?':'}','Q':'|','[':'A','H':'S','k':'F','|':'H','L':'J','B':'K','V':'L','z':'Z','R':'X','o':'C','(':'V','y':'B','}':'N','#':'M','m':'<','v':'>','G':'?'};
 
+function codeenc(message){
+    let encrypted="";
+    for(let c of message){
+        encrypted += keys[c];
+    }
+    return encrypted;
+}
+
+function codedec(message){
+    let decrypted="";
+    for(let c of message){
+        decrypted += symbols[c];
+    }
+    return decrypted;
+}
+
+
+
 function encrypt(){
-    let message = document.getElementById("normalmessage").value;
-    if(message.length === 0){
+    let encrypted= document.getElementById("normalmessage").value;
+    let code=Number(document.getElementById("noofenc").value);
+    if(encrypted.length === 0){
         document.getElementById("emptymessage").innerHTML="EMPTY INPUT...!";
         return;
     }
-    let encrypted = "";
-    for(let c of message){
-        encrypted += keys[c];
+    if(code== 0){
+        document.getElementById("emptymessage").innerHTML="EMPTY CODE...!";
+        return;
+    }
+    for(let i=0;i<code;i++){
+        encrypted=codeenc(encrypted);
     }
     document.getElementById("normalmessage").value = encrypted;
 }
 function decrypt(){
-    let message = document.getElementById("normalmessage").value;
-    if(message.length === 0){
+    let decrypted= document.getElementById("normalmessage").value;
+    let code=Number(document.getElementById("noofenc").value);
+    if(decrypted.length == 0){
         document.getElementById("emptymessage").innerHTML="EMPTY INPUT...!";
         return;
     }
-    let decrypted="";
-    for(let c of message){
-        decrypted += symbols[c];
+    if(code== 0){
+        document.getElementById("emptymessage").innerHTML="EMPTY CODE...!";
+        return;
+    }
+    for (let i=0;i<code;i++){
+        decrypted=codedec(decrypted);
     }
     document.getElementById("normalmessage").value = decrypted;
 }
